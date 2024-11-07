cff82850ee6f65d60afb69ebac7d998984e23a7c
Vharun2005 <vharunsivaraj06@gmail.com>
2024-11-07 20:11:23 +0530

    Update ConnectDataBase.js

--- backend/data/ConnectDataBase.js
+++ backend/data/ConnectDataBase.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDatabase = async() =>{
-    await mongoose.connect('mongodb://localhost:/gemini')
+    await mongoose.connect('mongodb+srv://vharun06:vharun%402005@vharundatabase.myfuz.mongodb.net/gemini')
     console.log('database connected')
 }
 
